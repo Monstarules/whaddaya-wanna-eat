f1e34d1b29b2db3137bd1ea54ce10efa8141b219
Brandon <brubio3724@gmail.com>
2021-10-26 17:52:39 -0400

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connect = require('./config/database')
+
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.use('/', (req, res) => {
-    res.send("hello")
-})
+const start = async () => {
+    try {
+        await connect()
+        app.listen(port, () => console.log(`Listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+start()
