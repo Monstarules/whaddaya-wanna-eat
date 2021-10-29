db68aa9e01a49682fe9702ce1a200fbec2b3032a
Brandon <brubio3724@gmail.com>
2021-10-29 18:00:25 -0400

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const express = require('express')
 const app = express()
 
 const connect = require('./config/database')
+const users = require('./routes/users.routes.js')
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.use('/api/users', users)
 
 const start = async () => {
     try {
