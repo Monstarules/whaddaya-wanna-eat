728f47e905243d9beab2ce98f605c8ea0599d46e
Dylan roo <dreuterpro@gmail.com>
2021-11-02 13:42:48 -0400

    Changed Party Schema to include Resturant List & changed party api to include party code

--- server/controllers/party.controllers.js
+++ server/controllers/party.controllers.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const router = express.Router()
+
 const Party = require('../models/PartySchema')
 
-// Create a Party (take in User id) WORKING
-router.post('/:id', async (req, res) => {
+// Create a Party (take in User id) 
+const createParty = async (req, res) => {
     
     // Setup new Party
-    const party = new Party
+    const party = new Party ({
+        code: req.body.code
+    })
 
     // Push the User's ID into the party array
     party.User_IDs.push(req.params.id)
@@ -18,10 +19,10 @@ router.post('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json({message: 'Error'})
     }
-})
+}
 
-// Delete Party (take in Party id) WORKING
-router.delete('/:partyid', async(req, res) => {
+// Delete Party (take in Party id) 
+const deleteParty = async(req, res) => {
     try {
         await Party.findByIdAndDelete(req.params.partyid)
         res.status(200).json({message: 'Party deleted'})
@@ -29,25 +30,25 @@ router.delete('/:partyid', async(req, res) => {
         res.status(500).json({message: 'Error'})
     }
 
-})
+}
 
-// Join Party (take in party id & user id)   
-router.patch('/:partyid/:userid', async (req, res) => {
+// Join Party (take in party CODE & user id)   
+const joinParty = async (req, res) => {
     
-    const partyid = req.params.partyid
+    const partyCode = req.params.code
     const userid = req.params.userid
 
     try {
-        await Party.findByIdAndUpdate({_id: partyid}, {$push: {User_IDs: userid} },{'new':true})
+        await Party.findOneAndUpdate({code: partyCode}, {$push: {User_IDs: userid} },{'new':true})
         res.status(200).json({message: 'Party Joined'})
     }
     catch (err) {
         res.status(500).json({message: 'Error'})
     }
-})
+}
 
 // Leave Party (take in party id & user id)
-router.delete('/:partyid/:userid', async (req, res) => {
+const leaveParty = async (req, res) => {
 
     const partyid = req.params.partyid
     const userid = req.params.userid
@@ -59,6 +60,28 @@ router.delete('/:partyid/:userid', async (req, res) => {
     catch (err) {
         res.status(500).json({message: 'Error'})
     }
-})
+}
 
-module.exports = router
+// Add to temp resturant list
+const addList = async (req, res) => {
+
+    const partyCode = req.params.code
+    const resturantName = req.body.resturantName
+
+    try {
+        await Party.findOneAndUpdate({code: partyCode}, {$push: {resturant_List: resturantName} },{'new':true})
+        res.status(200).json({message: 'Resturant Added'})
+    } catch (err) {
+        res.status(500).json({message: 'Error'})
+    }
+}
+
+
+
+module.exports = {
+    createParty,
+    deleteParty,
+    joinParty,
+    leaveParty,
+    addList
+}
--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const app = express()
 
 const connect = require('./config/database')
 const users = require('./routes/users.routes.js')
+const Partys = require('./routes/party.routes.js')
 
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.use('/api/users', users)
+
+app.use('/Party', Partys)
+
+
 
 const start = async () => {
     try {
