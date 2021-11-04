a2d659431f27fb42658e8459d8330bd73c7e8dd7
Brandon <brubio3724@gmail.com>
2021-11-04 19:00:45 -0400

    Added verifyAccount API
    Created endpoint for verifyAccount and added verification for the login endpoint

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,12 +1,24 @@
 const jwt = require('jsonwebtoken')
+const sendConfirmationEmail = require('../config/nodemailer')
 const User = require('../models/User')
 
 const register = async (req, res) => {
     try {
         const user = await User.create(req.body)
+        
+        if (!user) {
+            return res.status(500).json({ status: 'failure', message: 'User object is undefined'})
+        }
+
+        const username = user['username']
+        const email = user['email']
+        const code = user['confirmation_code']
+       
+        sendConfirmationEmail(username, email, code)
+        
         res.status(201).json({ status: 'success'})
     } catch (error) {
-        res.status(500).json({ status: 'failure', msg: error })
+        res.status(500).json({ status: 'failure', message: error })
     }
 }
 
@@ -15,13 +27,17 @@ const login = async (req, res) => {
         const user = await User.findOne({ username: req.body.username })
 
         if (user.length === 0) {
-            return res.status(404).json({ msg: "user does not exist" })
+            return res.status(404).json({ message: "User does not exist" })
+        }
+
+        if (user['status'] != 'Active') {
+            return res.status(401).json({ message: 'Pending account'})
         }
 
         const isPassword = await user.comparePassword(req.body.password)
 
         if (!isPassword) {
-            return res.status(500).json({ msg: 'wrong password'})
+            return res.status(500).json({ message: 'Wrong password'})
         }
 
         const userObject = Object.values(user)
@@ -29,11 +45,26 @@ const login = async (req, res) => {
 
         res.status(200).json({ status: 'success', token: token })
     } catch (error) {
-        res.status(500).json({ status: 'failue', msg: error })
+        res.status(500).json({ status: 'failue', message: error })
+    }
+}
+
+const verifyAccount = async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate({ confirmationCode: req.params.confirmationCode}, { status: 'Active' })
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        res.status(200).json({ status: 'success' })
+    } catch (error) {
+        res.status(500).json({ message: error})
     }
 }
 
 module.exports = {
     register,
-    login
+    login,
+    verifyAccount
 }
