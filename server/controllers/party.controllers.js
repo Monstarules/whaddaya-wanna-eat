6ebd0d302f4004bae18b8b6a97ac3b7b39ca8d49
andregr13 <89662452+andregr13@users.noreply.github.com>
2021-11-09 13:32:20 -0500

    Update party.controllers.js
    Adds random code gen and changes add restaurant to find by id instead of code

--- server/controllers/party.controllers.js
+++ server/controllers/party.controllers.js
@@ -4,18 +4,20 @@ const Party = require('../models/PartySchema')
 // Create a Party (take in User id) 
 const createParty = async (req, res) => {
     
-    // Setup new Party
-    const party = new Party ({
-        code: req.body.code
-    })
-
-    // Push the User's ID into the party array
-    party.User_IDs.push(req.params.id)
-
     try {
+        var code
+        
+        // Setup new Party
+        const party = new Party ({
+            code = generateCode()
+        })
+        
+        // Push the User's ID into the party array
+        party.User_IDs.push(req.params.id)
+    
         // save party to database
         const newParty =  await party.save()
-        res.status(201).json({message: 'Party Created'})
+        res.status(201).json({partyCode: code, message: 'Party Created'})
     } catch (err) {
         res.status(500).json({message: 'Error'})
     }
@@ -65,11 +67,11 @@ const leaveParty = async (req, res) => {
 // Add to temp resturant list
 const addList = async (req, res) => {
 
-    const partyCode = req.params.code
+    const partyid = req.params.partyid
     const resturantName = req.body.resturantName
 
     try {
-        await Party.findOneAndUpdate({code: partyCode}, {$push: {resturant_List: resturantName} },{'new':true})
+        await Party.findIdAndUpdate({_id: partyid}, {$push: {resturant_List: resturantName} },{'new':true})
         res.status(200).json({message: 'Resturant Added'})
     } catch (err) {
         res.status(500).json({message: 'Error'})
@@ -77,6 +79,33 @@ const addList = async (req, res) => {
 }
 
 
+
+function generateCode(){
+	// all possible characters for codegen
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    
+	// initialize return string
+	var retval = ""
+	
+	// gives 10,000 tries at randomly generating unique code
+	for(var i = 0; i<10000; i++){
+		
+		// loop creating a 6 char string
+		for(j = 0; j<6; j++){
+			var retval = retval + chars[Math.floor(Math.random()*36)]
+		}
+		
+		// check to ensure code is unique
+		var check = Party.findOne({code: retval})
+		
+		if(check === null){
+			return retval
+		}
+	}
+	
+	// unable to generate unique code
+	return null
+}
 
 module.exports = {
     createParty,
