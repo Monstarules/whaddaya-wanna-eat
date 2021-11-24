3c5fa46c5dfb7feb55dbd90cc232850308475607
Dylan roo <dreuterpro@gmail.com>
2021-11-23 21:46:21 -0500

    teehee

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/authenicate')
 
 const { register, login, verifyAccount, sendPasswordReset, resetPassword, editProfile, getProfile } = require('../controllers/users.controllers')
 
