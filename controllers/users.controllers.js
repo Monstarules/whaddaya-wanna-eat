65cc12f1bb78d073a4fec55c217e714d12489082
Dylan roo <dreuterpro@gmail.com>
2021-11-23 23:04:03 -0500

    spelling errors again? nah

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -123,7 +123,7 @@ const getProfile = async (req, res) => {
     const userid = req.user.user_id
 
     try {
-        const user = await User.findbyId(userid).exec()
+        const user = await User.findById(userid)
         
         if (!user)
         {
