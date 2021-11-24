b94fe89fb2a11a369c5bdadb985018a90abe71e6
Dylan roo <dreuterpro@gmail.com>
2021-11-23 21:50:08 -0500

    very serious update

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/authenicate')
+const auth = require('../middleware/authenticate')
 
 const { register, login, verifyAccount, sendPasswordReset, resetPassword, editProfile, getProfile } = require('../controllers/users.controllers')
 
