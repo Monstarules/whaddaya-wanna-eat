0e9315107ec2cd88802b049f3288114bf2068e60
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 10:46:20 -0500

    Update index.js

--- app/src/components/Verification/index.js
+++ app/src/components/Verification/index.js
@@ -16,7 +16,7 @@ const VERIFY = (id) => {
 const Verification = () => {
     return (
         <div className="things">
-	    <img src="" alt="" onload={VERIFY} style={{display: none;}}/>
+	    <img src="" alt="" onload={VERIFY} style={{display: none}}/>
             <h1>Thank you for choosing us!</h1>
             <p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
             <br /><br />
