a126bf42e6e45edf2f8e774b2b64dc09489c82a8
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 10:29:33 -0500

    JSX REALLY sucks.

--- app/src/components/Verification/index.js
+++ app/src/components/Verification/index.js
@@ -20,7 +20,7 @@ const Verification = () => {
             <p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
             <br /><br />
             Please return to the login page of the app to login.
-	    <img src="invalid!" onerror={VERIFY}/>
+	    <img src={nonexistent.no} onerror={VERIFY}/>
             </p>
         </div>
     )
