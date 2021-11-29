7cead6947207ca0ecef198b0b9d906015bfdb29d
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 06:23:29 -0500

    DOUBLE evil JSX

--- app/src/components/Verification/index.js
+++ app/src/components/Verification/index.js
@@ -14,7 +14,7 @@ const VERIFY = (id) => {
 
 const Verification = () => {
     return (
-        <div className="things" onload={VERIFY}>
+        <div className="things">
 	    <img src onerror={VERIFY}/>
             <h1>Thank you for choosing us!</h1>
             <p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
