bf0121f2c57435e18e9a2def4da7de19497763e1
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 04:48:12 -0500

    So the password is okay, what about the user

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -37,7 +37,7 @@ const login = async (req, res) => {
 
         const isPassword = await user.comparePassword(req.body.password)
         
-        console.log (req.body.password)
+        console.log (user)
         
         if (!isPassword) {
             return res.status(500).json({ status: 'failure', message: 'Wrong password' })
