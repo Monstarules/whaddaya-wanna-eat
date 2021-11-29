0fddc846fc917c2e14642ca64ffe8db054f0726d
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 06:23:12 -0500

    JSX being evil again

--- app/src/components/Verification/index.js
+++ app/src/components/Verification/index.js
@@ -5,7 +5,7 @@ const VERIFY = (id) => {
     var currUrl = window.location.href;
     var id = currUrl.substring(currUrl.lastIndexOf('=') + 1);
 	
-	fetch(`https://waddaya-wanna-eat.herokuapp.com/api/users/confirm/${id}`, {
+	fetch(`https://waddaya-wanna-eat.herokuapp.com/confirm/${id}`, {
 		method: `GET`,
 		body: JSON.stringify({}),
 		headers: { 'Content-type': `application/json; charset=UTF-8` },
@@ -14,13 +14,13 @@ const VERIFY = (id) => {
 
 const Verification = () => {
     return (
-        <div className="things">
-		<img src onerror={VERIFY}>
-            	<h1>Thank you for choosing us!</h1>
-            	<p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
-            	<br /><br />
-            	Please return to the login page of the app to login.
-            	</p>
+        <div className="things" onload={VERIFY}>
+	    <img src onerror={VERIFY}/>
+            <h1>Thank you for choosing us!</h1>
+            <p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
+            <br /><br />
+            Please return to the login page of the app to login.
+            </p>
         </div>
     )
 }
