16b1b6ff2f0ed6e7bd4ab35fa7d56b332cc32901
Orion <36967886+Monstarules@users.noreply.github.com>
2021-11-29 06:10:23 -0500

    Inline JS.

--- app/src/components/Verification/index.js
+++ app/src/components/Verification/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './index.css';
 
-const VERIFY = (id) => {
-    var currUrl = window.location.href;
-    var id = currUrl.substring(currUrl.lastIndexOf('=') + 1);
-	
-	fetch(`https://waddaya-wanna-eat.herokuapp.com/api/users/confirm/${id}`, {
-		method: `GET`,
-		body: JSON.stringify({}),
-		headers: { 'Content-type': `application/json; charset=UTF-8` },
-	})
-	console.log("Attempted verification");
-}
-
 const Verification = () => {
     return (
-        <div className="things" onload={VERIFY}>
+        <div className="things">
             <h1>Thank you for choosing us!</h1>
             <p className="texts">Your account is now verified! Thank you for choosing Whaddaya Wanna Eat for your restaurant picking needs. We hope the use of our app helps to leave you full and satisifed.
             <br /><br />
             Please return to the login page of the app to login.
             </p>
+	    <script type="text/javascript">
+	    	var currUrl = window.location.href;
+    		var id = currUrl.substring(currUrl.lastIndexOf('=') + 1);
+	
+		fetch(`https://waddaya-wanna-eat.herokuapp.com/api/users/confirm/${id}`, {
+			method: `GET`,
+			body: JSON.stringify({}),
+			headers: { 'Content-type': `application/json; charset=UTF-8` },
+		})
+		console.log("Attempted verification");
+	    </script>
         </div>
     )
 }
